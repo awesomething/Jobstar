9240a0cdad5712b1b69a4b17e107cb72af86bbb1
Awesome Henry <henryebomah@gmail.com>
2021-04-19 16:35:53 -0700

    Create Banner.test.js

---
+++ src/tests/Banner.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Banner from '../components/Banner'
+
+describe(`Banner component`, () => {
+  const props = {
+    id: 'a',
+    name: 'test-class-name',
+    
+  }
+
+  it('renders a .Banner by default', () => {
+    const wrapper = shallow(<Banner />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders the Banner given props', () => {
+    const wrapper = shallow(<Banner {...props} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
