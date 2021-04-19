89cd23d393d46c79a902b22d660a2368b34483fa
Awesome Henry <henryebomah@gmail.com>
2021-04-19 16:43:05 -0700

    Create Search.test.js

---
+++ src/tests/Search.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Search from '../components/Search'
+
+describe(`Search component`, () => {
+  const props = {
+    id: 'a',
+    name: 'test-class-name',
+    
+  }
+
+  it('renders a .Search by default', () => {
+    const wrapper = shallow(<Search />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders the Search given props', () => {
+    const wrapper = shallow(<Search {...props} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
