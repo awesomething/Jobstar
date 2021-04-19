98dcd402320582de2454da188481ecbc9b98fd0d
Awesome Henry <henryebomah@gmail.com>
2021-04-19 16:39:09 -0700

    Create Footer.test.js

---
+++ src/tests/Footer.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Footer from '../components/Footer'
+
+describe(`Footer component`, () => {
+  const props = {
+    id: 'a',
+    name: 'test-class-name',
+    
+  }
+
+  it('renders a .Footer by default', () => {
+    const wrapper = shallow(<Footer />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders the Footer given props', () => {
+    const wrapper = shallow(<Footer {...props} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
